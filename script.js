87b9daff0ac38e3d5f5de57428777892cb031efb
PRINCE KESHRI <cs21b1023@iiitr.ac.in>
2024-07-24 23:06:19 +0530

    Update landing page animation

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var t1 = gsap.timeline();
+
+// opening page animation
+
 t1.to("#loader", { 
     y: -300,
     x:200,
-    duration:1.5,
+    duration:0.9,
     scale:2,
-    delay: 0.5,
+    delay: 0.4,
     ease: "power2.out",
     opacity: 0,
     onComplete: () => {
@@ -12,11 +15,11 @@ t1.to("#loader", {
         }
 }); 
 
- t1.to("#start1", { 
+t1.to("#start1", { 
         height: "0vh",
         width: "0vw",
-        duration: 1,
-        ease: "power3.inOut",
+        duration: 0.8,
+        ease: "power2.Out",
         opacity: 0,
         borderRadius: "50%", 
         transformOrigin: "center center", 
@@ -26,52 +29,61 @@ t1.to("#loader", {
 });
     
 
+// navbar animation
+t1.from(".logo-container , .nav-items1, .nav-items", {
+    y: -18,
+    opacity: 0,
+    duration: 1.5,
+    delay: 0.1,
+    ease: "power3.out",
+    stagger:0.08,
+})
 
-t1.from(".logo-container", {
-    y: -20,
-    opacity: 0,
-    duration: 0.6,
-    delay: 0.3,
-    ease: "power3.out"
-})
-t1.from(".nav-items", {
-    y: -20,
-    opacity: 0,
-    duration: 0.6,
-    // delay: 0.2,
-    stagger: 0.3,
-    ease: "power3.out"
-})
-t1.from(".nav-items1", {
-    y: -20,
-    opacity: 0,
-    duration: 0.6,
-    // delay: 0.2,
-    stagger: 0.5,
-    ease: "power3.out"
-})
+// hero page animation
 t1.from(".imgContainer", {
     x: -200,
     opacity: 0,
-    duration: 0.5,
+    duration: 0.7,
     rotate:360,
     ease: "power2.out"
-})
+},"abc")
 t1.from(".infoContainer", {
-    x: 1600,
-    opacity: 0.5,
-    duration: 1,
+    x: 200,
+    opacity: 0,
+    duration: 0.7,
     ease: "power3.out"
-})
-// t1.to(".projectContainer", {
-//     transform:"translatex(140%)",
-//     duration: 1,
-//     ease: "power3.out",
-//     stagger:0.3,
-//     scrollTrigger: {
-//         trigger: "#projects",
-//         scroller: "body",
-//         scrub: 2,
-//         pin:true,
-//     }
-// })
+},"abc")
+
+// faded text on landing page
+var faded = document.querySelector(".fadedText.absolute");
+var fade = faded.textContent;
+var fadedArray = fade.split("");
+var fadedlength = fadedArray.length / 2;
+var clutter = ""
+
+fadedArray.forEach(function (ele, ind) {
+    if (ind < fadedlength) {
+        clutter += `<span class='ac'>${ele}</span>`;
+    }
+    else {
+        clutter += `<span class='bc'>${ele}</span>`;
+    }
+    faded.innerHTML = clutter
+});
+
+t1.from(".ac", {
+    y: -100,
+    bottom:"28%",
+    duration: 0.7,
+    stagger: 0.17,
+    opacity: 0,
+    
+},"abc")
+t1.from(".bc", {
+    y: -100,
+    bottom:"28%",
+    duration: 0.7,
+    stagger: -0.17,
+    opacity: 0,
+    
+},"abc")
